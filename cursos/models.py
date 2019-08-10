07e6a9f7997b4056664a85b2911c6b0458788921
rodrigoazs <rodrigo4zevedo@gmail.com>
2019-08-10 18:31:54 -0300

    Changed on_delete in models

--- cursos/models.py
+++ cursos/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from sort_order_field import SortOrderField
 
 class Apostila(models.Model):
     titulo = models.CharField(max_length=255)
@@ -16,7 +15,7 @@ class Curso(models.Model):
 class Aula(models.Model):
     titulo = models.CharField(max_length=255)
     apostilas = models.ManyToManyField(Apostila, blank=True, related_name='aulas', through='Pacote')
-    curso = models.ForeignKey(Curso, blank=True, null=True, on_delete=None)
+    curso = models.ForeignKey(Curso, blank=True, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.titulo
@@ -25,8 +24,8 @@ class Pacote(models.Model):
     class Meta:
         ordering =  ('ordem',)
 
-    apostila = models.ForeignKey(Apostila, on_delete=None)
-    aula = models.ForeignKey(Aula, on_delete=None)
+    apostila = models.ForeignKey(Apostila, on_delete=models.PROTECT)
+    aula = models.ForeignKey(Aula, on_delete=models.PROTECT)
     ordem = models.PositiveIntegerField(default=0)
 
     def __str__(self):
