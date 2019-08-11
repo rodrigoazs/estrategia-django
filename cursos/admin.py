8ee10d92119e9816e65b297375365deed312a580
rodrigoazs <rodrigo4zevedo@gmail.com>
2019-08-11 11:04:20 -0300

    Modified models

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from adminsortable2.admin import SortableInlineAdminMixin
 from .models import Apostila, Curso, Aula
 
@@ -30,8 +31,14 @@ class AulaAdmin(admin.ModelAdmin):
     list_filter = ['created_at', 'updated_at']
 
 class CursoAdmin(admin.ModelAdmin):
+    def image_tag(self, obj):
+        return format_html('<img src="/{}" />'.format(obj.imagem.url))
+
+    image_tag.short_description = 'Imagem'
+
     inlines = [AulaInline]
     search_fields = ['titulo']
+    list_display = ['titulo', 'image_tag', 'created_at', 'updated_at']
     list_filter = ['created_at', 'updated_at']
 
 admin.site.register(Apostila, ApostilaAdmin)
--- cursos/models.py
+++ cursos/models.py
@@ -1,25 +1,27 @@
 from django.db import models
 
 class Apostila(models.Model):
-    titulo = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    titulo = models.CharField('Título', max_length=255)
+    created_at = models.DateTimeField('Criação', auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField('Atualização', auto_now=True, editable=False)
 
     def __str__(self):
         return self.titulo
 
 class Curso(models.Model):
-    titulo = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    titulo = models.CharField('Título', max_length=255)
+    descricao = models.TextField('Descrição', max_length=500)
+    imagem = models.ImageField(upload_to='static/images/cursos/%Y/%m', max_length=100)
+    created_at = models.DateTimeField('Criação', auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField('Atualização', auto_now=True, editable=False)
 
     def __str__(self):
         return self.titulo
 
 class Aula(models.Model):
-    titulo = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    titulo = models.CharField('Título', max_length=255)
+    created_at = models.DateTimeField('Criação', auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField('Atualização', auto_now=True, editable=False)
     apostilas = models.ManyToManyField(Apostila, blank=True, related_name='aulas', through='Pacote')
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
 
@@ -32,7 +34,7 @@ class Pacote(models.Model):
 
     apostila = models.ForeignKey(Apostila, on_delete=models.CASCADE)
     aula = models.ForeignKey(Aula, on_delete=models.CASCADE)
-    ordem = models.PositiveIntegerField(default=0)
+    ordem = models.PositiveIntegerField(default=0, blank=False, null=False)
 
     def __str__(self):
         return self.apostila.titulo
