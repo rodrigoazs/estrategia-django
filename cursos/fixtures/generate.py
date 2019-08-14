3540c057b6d5799d09655dd9d2ad71f1b1b67594
Rodrigo Azevedo <rodrigo4zevedo@gmail.com>
2019-08-14 10:39:34 -0300

    Sent final fixtures generator

--- cursos/fixtures/generate.py
+++ cursos/fixtures/generate.py
@@ -2,21 +2,26 @@ import os
 import lorem
 import json
 import datetime
+import random
 import urllib.request
 
 LOREM_PICSUM = 'https://picsum.photos/200/150/?blur=2'
 STATIC_PATH = 'static/images/cursos/{}/{}'
 STATIC_URL = 'static/images/cursos/{}/{}/{}.jpg'
 CURSOS_SIZE = 20
+AULAS_SIZE = 70
+APOSTILAS_SIZE = 50
+PACOTES_SIZE = 300
 
 def generate():
     data = []
     current_time = datetime.datetime.now()
+    year = current_time.strftime('%Y')
+    month = current_time.strftime('%m')
+    dir_path = '../../' + STATIC_PATH.format(year, month)
 
+    # generando cursos
     for pk in range(1, CURSOS_SIZE+1):
-        year = current_time.strftime('%Y')
-        month = current_time.strftime('%m')
-        dir_path = '../../' + STATIC_PATH.format(year, month)
         image_path = STATIC_URL.format(year, month, pk)
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
@@ -32,7 +37,46 @@ def generate():
                      }
                 }
         data.append(model)
-        print('Generated curso {}'.format(pk))
+        print('Gerado curso {}'.format(pk))
+
+    # gerando aulas
+    for pk in range(1, AULAS_SIZE+1):
+        model = {'model': 'cursos.aula',
+                 'pk': pk,
+                 'fields': {
+                    'titulo': ' '.join(lorem.sentence().split()[:2]),
+                    'curso': random.randint(1, CURSOS_SIZE),
+                    'created_at': str(current_time),
+                    'updated_at': str(current_time),
+                    }
+                 }
+        data.append(model)
+        print('Gerado aula {}'.format(pk))
+
+    # gerando apostilas
+    for pk in range(1, APOSTILAS_SIZE+1):
+        model = {'model': 'cursos.apostila',
+                 'pk': pk,
+                 'fields': {
+                    'titulo': ' '.join(lorem.sentence().split()[:3]),
+                    'created_at': str(current_time),
+                    'updated_at': str(current_time),
+                    }
+                 }
+        data.append(model)
+        print('Gerado apostila {}'.format(pk))
+
+    # gerando pacotes
+    for pk in range(1, PACOTES_SIZE+1):
+        model = {'model': 'cursos.pacote',
+                 'pk': pk,
+                 'fields': {
+                    'apostila': random.randint(1, APOSTILAS_SIZE),
+                    'aula': random.randint(1, AULAS_SIZE),
+                    }
+                 }
+        data.append(model)
+        print('Gerado pacote {}'.format(pk))
 
     with open('cursos.json', 'w') as f:
         json.dump(data, f, indent=4)
